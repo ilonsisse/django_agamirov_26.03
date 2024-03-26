509b8e354d108f0d8fa349195cc23df86c65209b
Evgenii Istomin <ilonsisse@yandex.ru>
2024-03-26 18:00:44 +0300

    objects = models.Manager() added in models.py

--- postgres_kr/example/models.py
+++ postgres_kr/example/models.py
@@ -8,6 +8,7 @@ class Place(models.Model):
 
     name = models.CharField(max_length=100, verbose_name='Наименование')
     population = models.IntegerField(verbose_name='Население')
+    objects = models.Manager()
 
     def __str__(self):
         return f'Район {self.name}'
@@ -24,6 +25,7 @@ class Shop(models.Model):
     supervisor = models.CharField(max_length=64, verbose_name='Руководитель', blank=True, null=True)
     place = models.ForeignKey(to=Place, on_delete=models.PROTECT, verbose_name='Район')
     is_open = models.BooleanField(default=True, verbose_name='Открыт')
+    objects = models.Manager()
 
     def __str__(self):
         return f'Магазин {self.name}'
